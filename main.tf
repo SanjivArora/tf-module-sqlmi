d100593e1aefa4baf4ed1323dfc4b8e8de46b85e
Sharan Nagulapalli (healthAlliance) <Sharan.Nagulapalli@healthalliance.co.nz>
2023-06-20 16:08:41 +1200

    fix: remove unwanted nsg rules

--- main.tf
+++ main.tf
@@ -43,47 +43,47 @@ resource "azurerm_network_security_group" "sql_nsg" {
   )
 }
 
-resource "azurerm_network_security_rule" "allow_management_inbound" {
-  name                        = "allow_management_inbound"
-  priority                    = 106
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_ranges     = ["9000", "9003", "1438", "1440", "1452"]
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = local.resource_group_name
-  network_security_group_name = azurerm_network_security_group.sql_nsg.name
-}
+# resource "azurerm_network_security_rule" "allow_management_inbound" {
+#   name                        = "allow_management_inbound"
+#   priority                    = 106
+#   direction                   = "Inbound"
+#   access                      = "Allow"
+#   protocol                    = "Tcp"
+#   source_port_range           = "*"
+#   destination_port_ranges     = ["9000", "9003", "1438", "1440", "1452"]
+#   source_address_prefix       = "*"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = local.resource_group_name
+#   network_security_group_name = azurerm_network_security_group.sql_nsg.name
+# }
 
-resource "azurerm_network_security_rule" "allow_misubnet_inbound" {
-  name                        = "allow_misubnet_inbound"
-  priority                    = 200
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "10.0.0.0/24"
-  destination_address_prefix  = "*"
-  resource_group_name         = local.resource_group_name
-  network_security_group_name = azurerm_network_security_group.sql_nsg.name
-}
+# resource "azurerm_network_security_rule" "allow_misubnet_inbound" {
+#   name                        = "allow_misubnet_inbound"
+#   priority                    = 200
+#   direction                   = "Inbound"
+#   access                      = "Allow"
+#   protocol                    = "*"
+#   source_port_range           = "*"
+#   destination_port_range      = "*"
+#   source_address_prefix       = "10.0.0.0/24"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = local.resource_group_name
+#   network_security_group_name = azurerm_network_security_group.sql_nsg.name
+# }
 
-resource "azurerm_network_security_rule" "allow_health_probe_inbound" {
-  name                        = "allow_health_probe_inbound"
-  priority                    = 300
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "AzureLoadBalancer"
-  destination_address_prefix  = "*"
-  resource_group_name         = local.resource_group_name
-  network_security_group_name = azurerm_network_security_group.sql_nsg.name
-}
+# resource "azurerm_network_security_rule" "allow_health_probe_inbound" {
+#   name                        = "allow_health_probe_inbound"
+#   priority                    = 300
+#   direction                   = "Inbound"
+#   access                      = "Allow"
+#   protocol                    = "*"
+#   source_port_range           = "*"
+#   destination_port_range      = "*"
+#   source_address_prefix       = "AzureLoadBalancer"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = local.resource_group_name
+#   network_security_group_name = azurerm_network_security_group.sql_nsg.name
+# }
 
 resource "azurerm_network_security_rule" "allow_tds_inbound" {
   name                        = "allow_tds_inbound"
@@ -99,61 +99,61 @@ resource "azurerm_network_security_rule" "allow_tds_inbound" {
   network_security_group_name = azurerm_network_security_group.sql_nsg.name
 }
 
-resource "azurerm_network_security_rule" "deny_all_inbound" {
-  name                        = "deny_all_inbound"
-  priority                    = 4096
-  direction                   = "Inbound"
-  access                      = "Deny"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = local.resource_group_name
-  network_security_group_name = azurerm_network_security_group.sql_nsg.name
-}
+# resource "azurerm_network_security_rule" "deny_all_inbound" {
+#   name                        = "deny_all_inbound"
+#   priority                    = 4096
+#   direction                   = "Inbound"
+#   access                      = "Deny"
+#   protocol                    = "*"
+#   source_port_range           = "*"
+#   destination_port_range      = "*"
+#   source_address_prefix       = "*"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = local.resource_group_name
+#   network_security_group_name = azurerm_network_security_group.sql_nsg.name
+# }
 
-resource "azurerm_network_security_rule" "allow_management_outbound" {
-  name                        = "allow_management_outbound"
-  priority                    = 102
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_ranges     = ["80", "443", "12000"]
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = local.resource_group_name
-  network_security_group_name = azurerm_network_security_group.sql_nsg.name
-}
+# resource "azurerm_network_security_rule" "allow_management_outbound" {
+#   name                        = "allow_management_outbound"
+#   priority                    = 102
+#   direction                   = "Outbound"
+#   access                      = "Allow"
+#   protocol                    = "Tcp"
+#   source_port_range           = "*"
+#   destination_port_ranges     = ["80", "443", "12000"]
+#   source_address_prefix       = "*"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = local.resource_group_name
+#   network_security_group_name = azurerm_network_security_group.sql_nsg.name
+# }
 
-resource "azurerm_network_security_rule" "allow_misubnet_outbound" {
-  name                        = "allow_misubnet_outbound"
-  priority                    = 200
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "10.0.0.0/24"
-  destination_address_prefix  = "*"
-  resource_group_name         = local.resource_group_name
-  network_security_group_name = azurerm_network_security_group.sql_nsg.name
-}
+# resource "azurerm_network_security_rule" "allow_misubnet_outbound" {
+#   name                        = "allow_misubnet_outbound"
+#   priority                    = 200
+#   direction                   = "Outbound"
+#   access                      = "Allow"
+#   protocol                    = "*"
+#   source_port_range           = "*"
+#   destination_port_range      = "*"
+#   source_address_prefix       = "10.0.0.0/24"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = local.resource_group_name
+#   network_security_group_name = azurerm_network_security_group.sql_nsg.name
+# }
 
-resource "azurerm_network_security_rule" "deny_all_outbound" {
-  name                        = "deny_all_outbound"
-  priority                    = 4096
-  direction                   = "Outbound"
-  access                      = "Deny"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = local.resource_group_name
-  network_security_group_name = azurerm_network_security_group.sql_nsg.name
-}
+# resource "azurerm_network_security_rule" "deny_all_outbound" {
+#   name                        = "deny_all_outbound"
+#   priority                    = 4096
+#   direction                   = "Outbound"
+#   access                      = "Deny"
+#   protocol                    = "*"
+#   source_port_range           = "*"
+#   destination_port_range      = "*"
+#   source_address_prefix       = "*"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = local.resource_group_name
+#   network_security_group_name = azurerm_network_security_group.sql_nsg.name
+# }
 
 resource "azurerm_subnet_network_security_group_association" "sqlmi_association" {
   subnet_id                 = var.sqlmi_subnet_id
